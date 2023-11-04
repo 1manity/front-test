0d5e960dd31a9c15876a4b0980a547ac665d84e2
1manity <1052116648@qq.com>
2023-11-05 01:06:32 +0800

    20231105 nice try!

--- have-a-try/src/App.vue
+++ have-a-try/src/App.vue
@@ -4,12 +4,12 @@
 
     </div>
   </div>
-
 </template>
 <script setup>
 import {onMounted, ref} from 'vue';
 import { gsap } from 'gsap';
 import Test from "./component/test.vue";
+import Logo from "./assets/logo.png"
 
 const box = ref(null);
 const boxAnim =()=> {gsap.to(box.value, { x: 100, duration: 2 });}
@@ -23,16 +23,21 @@ onMounted(() => {
   position: relative;
   width: 100%;
   height: 100%;
-  background: #000;
+  background: #2980B9;  /* fallback for old browsers */
+  background: -webkit-linear-gradient(to right, #FFFFFF, #6DD5FA, #2980B9);  /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to right top, #FFFFFF, #6DD5FA, #2980B9); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
 }
 .anim-test {
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%,-50%);
-  width: 100px;
-  height: 100px;
-  background: red;
-  /* animation: anim 1s linear infinite; */
+  width: 500px;
+  height: 500px;
+  background-image: url("./assets/logo.png");
+  background-repeat: no-repeat;
+  background-size: contain;
+
 }
 </style>
